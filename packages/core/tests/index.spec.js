2f712dfb755decd7a271438b6742d2a72dc850a6
Lior <lior.heber@kenshoo.com>
2020-02-21 13:55:31 -0500

    add basic jest support

---
+++ packages/core/tests/index.spec.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import HelloWorld from "../src";
+import toJson from "enzyme-to-json";
+
+describe("core component", () => {
+  test("will render", () => {
+    const cmp = shallow(<HelloWorld />);
+    expect(toJson((cmp))).toMatchSnapshot();
+  });
+});
