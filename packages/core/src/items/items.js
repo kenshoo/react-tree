c38be44808545e580283777cf19f9921b7e2d91b
Lior <lior.heber@kenshoo.com>
2021-01-08 09:52:20 -0500

    Prepare for initial release

--- packages/core/src/items/items.js
+++ packages/core/src/items/items.js
@@ -3,15 +3,8 @@ import { jsx } from "@emotion/core";
 import React from "react";
 
 const ItemsRenderer = props => {
-  const {
-    getStyles,
-    children
-  } = props;
-  return (
-      <div css={getStyles("items", props)}>
-        {children}
-      </div>
-  );
+  const { getStyles, children } = props;
+  return <div css={getStyles("items", props)}>{children}</div>;
 };
 
 export default ItemsRenderer;
